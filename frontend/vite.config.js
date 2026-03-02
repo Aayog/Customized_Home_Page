2460535e83773ee23e1af84f3fd0f2ac814023e2
Aayog Koirala <1aayogkoirala@gmail.com>
2026-03-02 14:19:28 -0600

    Change base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/homelab-dashboard/',
+  base: '/Customized_Home_Page/',
   server: {
     proxy: {
       '/api': {
